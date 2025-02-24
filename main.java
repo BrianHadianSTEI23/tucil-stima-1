d16f1156db6cbfa51ee8e163de46a0ed1740cdcd
brihadianSTEI <19623254@std.stei.itb.ac.id>
2025-02-24 11:29:47 +0700

    chore : testing data

--- main.java
+++ main.java
@@ -153,137 +153,137 @@ public class main {
                         int nBlockPuzzle = 0;
                         Puzzle puzzle = MainMap.puzzleNotUsed(MainMap, puzzleList);
 
-                        
-                        // get how many block is filled by puzzle
-                        for (int i = 0; i < puzzle.getRows(); i++) {
-                            for (int j = 0; j < puzzle.getColumns(); j++) {
-                                if (puzzle.getElement(i, j) == '1') {
-                                    nBlockPuzzle++;
+                        // validation
+                        if (puzzle != null) {
+                            // get how many block is filled by puzzle
+                            for (int i = 0; i < puzzle.getRows(); i++) {
+                                for (int j = 0; j < puzzle.getColumns(); j++) {
+                                    if (puzzle.getElement(i, j) == '1') {
+                                        nBlockPuzzle++;
+                                    }
                                 }
                             }
-                        }
-                        
-                        // check are there enough empty blocks for puzzle
-                        if (MainMap.emptyBoxInMap() >= nBlockPuzzle && MainMap.isThereAValidPosition(MainMap, puzzle)) { 
-                            // trace each position for availability to be filled
-                            int k = 0;
-                            boolean found = false;
-                            while (!found && k < MainMap.getRows()) {
-                                int l = 0;
-                                while (!found && l < MainMap.getColumns()) {
-                                    if (MainMap.canBlockFit(k, l, MainMap, puzzle)) { // block dapat masuk
-                                        found = true;
-                                        MainMap.setMapAfterPuzzle(k, l, MainMap, puzzle);
-                                        break;
-                                    } else { // block cannot fit
-                                        int modifyPuzzleIter = 0;
-                                        
-                                        // check every possibility
-                                        while (modifyPuzzleIter < 8 && !found) {
-                                            puzzle.modifyPuzzle();
-                                            if (MainMap.canBlockFit(k, l, MainMap, puzzle)) { // block dapat masuk
-                                                found = true;
-                                                MainMap.setMapAfterPuzzle(k, l, MainMap, puzzle);
-                                            } else {
-                                                modifyPuzzleIter++;
+                            
+                            // check are there enough empty blocks for puzzle
+                            if (MainMap.emptyBoxInMap() >= nBlockPuzzle && MainMap.isThereAValidPosition(MainMap, puzzle)) { 
+                                // trace each position for availability to be filled
+                                int k = 0;
+                                boolean found = false;
+                                while (!found && k < MainMap.getRows()) {
+                                    int l = 0;
+                                    while (!found && l < MainMap.getColumns()) {
+                                        if (MainMap.canBlockFit(k, l, MainMap, puzzle)) { // block dapat masuk
+                                            found = true;
+                                            MainMap.setMapAfterPuzzle(k, l, MainMap, puzzle);
+                                        } else { // block cannot fit
+                                            int modifyPuzzleIter = 0;
+                                            
+                                            // check every possibility
+                                            while (modifyPuzzleIter < 8 && !found) {
+                                                puzzle.modifyPuzzle();
+                                                if (MainMap.canBlockFit(k, l, MainMap, puzzle)) { // block dapat masuk
+                                                    found = true;
+                                                    MainMap.setMapAfterPuzzle(k, l, MainMap, puzzle);
+                                                } else {
+                                                    modifyPuzzleIter++;
+                                                }
+                                            }
+                                            puzzle.modifyPuzzle(); // reset to first position
+    
+                                            // resetting variables
+                                            if (!found) {
+                                                // check other coordinate
+                                                l++;
                                             }
                                         }
-                                        puzzle.modifyPuzzle(); // reset to first position
-
-                                        // resetting variables
-                                        if (!found) {
-                                            // check other coordinate
-                                            l++;
+                                    }
+                                    if (!found) {
+                                        k++;
+                                    }
+                                }
+                            } else { // there are no enough space in main map
+                                MainMap.resetPuzzleMap();
+                            }
+                            
+                            // check if it's full
+                            if ((MainMap.getCharInMap().size() == puzzleList.length) && (MainMap.emptyBoxInMap() == 0)) {
+                                full = true;
+                            }
+                            operationIter++;
+                            
+                            // main debug
+                            // MainMap.getPuzzleMap();
+                            // System.out.println(MainMap.getCharInMap());
+                            
+                        }
+                    }
+                    
+                    // keeping the result as a .txt file
+                    if (full) {
+                        MainMap.getPuzzleMap();
+                        long timeStop = System.currentTimeMillis();
+                        System.out.println("Elapsed time : " + (timeStop - timeStart) + " ms.");
+                        System.out.println("Iteration tried : " + operationIter);     
+                        System.out.println("Want to save file : (y/n) ");
+                        String choice = inputScanner.nextLine();
+                        if (choice.equals("y")) {
+                            // bonus : converting into image
+                            System.out.println("Convert to image : (y/n) ");
+                            String convert = inputScanner.nextLine();
+                            System.out.println("Enter name to be saved: ");
+                            String fileSavedName = inputScanner.nextLine();
+                            if (convert.equals("y")) {
+                                // converting into image
+                                int width = MainMap.getColumns();
+                                int height = MainMap.getRows();
+                                List<Integer> colorHex = new ArrayList<>();
+                                Random random = new Random();
+                                for (int i = 0; i < puzzleList.length; i++) {
+                                    int color = random.nextInt(0xFFFFFF + 1);
+                                    colorHex.add(color);
+                                }
+    
+                                // Create a BufferedImage
+                                BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+    
+                                // Convert matrix to image pixels
+                                for (int y = 0; y < height; y++) {
+                                    for (int x = 0; x < width; x++) {
+                                        // searching index of character in charMap
+                                        boolean found = false;
+                                        int i = 0;
+                                        while (!found && i < MainMap.getCharInMap().size()) {
+                                            if (MainMap.getCharInMap().get(i) == MainMap.getElement(y, x)) {
+                                                image.setRGB(x, y, colorHex.get(i)); // Set pixel to black
+                                                found = true;
+                                            }
+                                            i++;
                                         }
                                     }
                                 }
-                                if (!found) {
-                                    k++;
+    
+                                // Save the image as a PNG file
+                                try {
+                                    int numberOfOutput = random.nextInt(10);
+                                    File output = new File("output" + numberOfOutput +".png");
+                                    ImageIO.write(image, "png", output);
+                                    System.out.println("Image saved as " + output + ".png");
+                                } catch (Exception e) {
+                                    e.printStackTrace();
                                 }
+                            } else {
+                                writeMatrixToFile(MainMap, fileSavedName);
                             }
-                        } else { // there are no enough space in main map
-                            MainMap.resetPuzzleMap();
+                        } else {
+                            System.out.println("Thank you for using this product.");
                         }
-                        
-                        // check if it's full
-                        if (MainMap.getCharInMap().size() == puzzleList.length) {
-                            full = true;
-                        }
-                        operationIter++;
-                        
-                        // main debug
-                        // MainMap.getPuzzleMap();
-                        // System.out.println(MainMap.getCharInMap());
-                    }
-                    
-                    // final check to sysout
-                    // System.out.println("jml kotak kosong : " + MainMap.emptyBoxInMap());
-                    if (MainMap.emptyBoxInMap() == 0) {
-                        MainMap.getPuzzleMap();
+                        inputScanner.close();
                     } else {
                         System.out.println("Brute force cannot be done.\nPlease try another file.");
                     }
-                    long timeStop = System.currentTimeMillis();
-                    System.out.println("Elapsed time : " + (timeStop - timeStart) + " ms.");
-                    System.out.println("Iteration tried : " + operationIter);     
 
-                    // keeping the result as a .txt file
 
                     
-                    // bonus : converting into image
-                    System.out.println("Want to save file : (y/n) ");
-                    String choice = inputScanner.nextLine();
-                    if (choice.equals("y")) {
-                        System.out.println("Convert to image : (y/n) ");
-                        String convert = inputScanner.nextLine();
-                        System.out.println("Enter name to be saved: ");
-                        String fileSavedName = inputScanner.nextLine();
-                        if (convert.equals("y")) {
-                            // converting into image
-                            int width = MainMap.getColumns();
-                            int height = MainMap.getRows();
-                            List<Integer> colorHex = new ArrayList<>();
-                            Random random = new Random();
-                            for (int i = 0; i < puzzleList.length; i++) {
-                                int color = random.nextInt(0xFFFFFF + 1);
-                                colorHex.add(color);
-                            }
-
-                            // Create a BufferedImage
-                            BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-
-                            // Convert matrix to image pixels
-                            for (int y = 0; y < height; y++) {
-                                for (int x = 0; x < width; x++) {
-                                    // searching index of character in charMap
-                                    boolean found = false;
-                                    int i = 0;
-                                    while (!found && i < MainMap.getCharInMap().size()) {
-                                        if (MainMap.getCharInMap().get(i) == MainMap.getElement(y, x)) {
-                                            image.setRGB(x, y, colorHex.get(i)); // Set pixel to black
-                                            found = true;
-                                        }
-                                        i++;
-                                    }
-                                }
-                            }
-
-                            // Save the image as a PNG file
-                            try {
-                                int numberOfOutput = random.nextInt(10);
-                                File output = new File("output" + numberOfOutput +".png");
-                                ImageIO.write(image, "png", output);
-                                System.out.println("Image saved as " + output + ".png");
-                            } catch (Exception e) {
-                                e.printStackTrace();
-                            }
-                        } else {
-                            writeMatrixToFile(MainMap, fileSavedName);
-                        }
-                    } else {
-                        System.out.println("Thank you for using this product.");
-                    }
-                    inputScanner.close();
                 } else {
                     System.out.println("No service as such. Please try again.");
                 } 
